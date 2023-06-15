bf557c1be90137db3709db59bef6ec43923e7693
Francesco Paglia <francesco98paglia@gmail.com>
2023-06-15 23:41:02 +0200

    Modifica test e deploy nell'ambiente di produzione

--- tests/test-containers.py
+++ tests/test-containers.py
@@ -1,36 +1,20 @@
 import requests
 
 # Controllo che il servizio create-container restituisca 'SUCCESSO'
-def test_1():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/create-container", json={"nomeContainer": "testpipeline", "accountStorage": "teststoragesrs"})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/create-container", json={"nomeContainer": "testpipeline", "accountStorage": "teststoragesrs"})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
 
 # Controllo che il servizio copy-files restituisca 'SUCCESSO'
-def test_2():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/copy-files", json={"nomeContainerOrigine": "$web", "accountStorageOrigine": "teststoragesrs", "nomeContainerDestinazione": "testpipeline", "accountStorageDestinazione": "teststoragesrs"})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-    response = requests.get("https://teststoragesrs.blob.core.windows.net/testpipeline/index.html")
-    assert response.status_code == 200
-    assert b"Sito prova" in response.content
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/copy-files", json={"nomeContainerOrigine": "$web", "accountStorageOrigine": "teststoragesrs", "nomeContainerDestinazione": "testpipeline", "accountStorageDestinazione": "teststoragesrs"})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
+response = requests.get("https://teststoragesrs.blob.core.windows.net/testpipeline/index.html")
+assert response.status_code == 200
+assert b"Sito prova" in response.content
 
 
 # Controllo che il servizio delete-container restituisca 'SUCCESSO'
-def test_3():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/delete-container", json={"nomeContainer":"testpipeline", "accountStorage": "teststoragesrs"})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-
-test_1()
-test_2()
-test_3()
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/delete-container", json={"nomeContainer":"testpipeline", "accountStorage": "teststoragesrs"})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
--- tests/test-website-lifecycle.py
+++ tests/test-website-lifecycle.py
@@ -1,71 +1,39 @@
 import requests
 
-# Controllo che il servizio check-name restituisca 'USATO'
-def test_1():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": "teststoragesrs"})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"USATO"
-
-# Controllo che il servizio check-name restituisca 'SUCCESSO'
-# Il nome del sito cambia in maniera tale da arrivare al successo del controllo
-def test_2(nomeSito):
-    esito = 503
-    while esito == 503:
-        disponibilita = b"USATO"
-        while disponibilita == b"USATO":
-            nomeSito += "srs"
-            response = requests.post("https://test-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
-            disponibilita = response.content
-            esito = response.status_code
-    assert esito == 200
-    assert disponibilita == b"DISPONIBILE"
-    return nomeSito
-
-# Controllo che il servizio create-account-storage restituisca 'SUCCESSO'
-def test_3():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/create-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-    
-# Controllo che il servizio enable-static-website restituisca 'SUCCESSO'
-def test_4():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/enable-static-website", json={"nomeSito": nomeSito})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-
-# Controllo che il servizio disable-static-website restituisca 'SUCCESSO'
-def test_5():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/disable-static-website", json={"nomeSito": nomeSito})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-
-# Controllo che il servizio delete-account-storage restituisca 'SUCCESSO'
-def test_6():
-    esito = 503
-    while esito == 503:
-        response = requests.post("https://test-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
-        esito = response.status_code
-    assert esito == 200
-    assert response.content == b"SUCCESSO"
-
 nomeSito = "testwebapp"
 gruppoRisorse = "test-env"
 
-test_1()
-nomeSito = test_2(nomeSito)
-test_3()
-test_4()
-test_5()
-test_6()
+# Controllo che il servizio check-name restituisca 'USATO'
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": "teststoragesrs"})
+assert response.status_code == 200
+assert response.content == b"USATO"
+
+# Controllo che il servizio check-name restituisca 'SUCCESSO'
+# Il nome del sito cambia in maniera tale da arrivare al successo del controllo
+disponibilita = b"USATO"
+while disponibilita == b"USATO":
+    nomeSito += "srs"
+    response = requests.post("https://test-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
+    disponibilita = response.content
+assert response.status_code == 200
+assert disponibilita == b"DISPONIBILE"
+
+# Controllo che il servizio create-account-storage restituisca 'SUCCESSO'
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/create-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
+
+# Controllo che il servizio enable-static-website restituisca 'SUCCESSO'
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/enable-static-website", json={"nomeSito": nomeSito})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
+
+# Controllo che il servizio disable-static-website restituisca 'SUCCESSO'
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/disable-static-website", json={"nomeSito": nomeSito})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
+
+# Controllo che il servizio delete-account-storage restituisca 'SUCCESSO'
+response = requests.post("https://test-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+assert response.status_code == 200
+assert response.content == b"SUCCESSO"
