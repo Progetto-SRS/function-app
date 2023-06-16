c9803b6f3d6878d4e99f4c91e0da4846c27d05af
Francesco Paglia <francesco98paglia@gmail.com>
2023-06-17 01:09:01 +0200

    Perfezionamento test del website lifecycle

--- tests/prod-test_website_lifecycle.py
+++ tests/prod-test_website_lifecycle.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-nomeSito = "testwebapp"
+nomeSito = "testwebappsrs"
 gruppoRisorse = "users-env"
 
 # Controllo che il servizio check-name restituisca 'USATO'
@@ -11,13 +11,12 @@ assert response.content == b"USATO"
 
 # Controllo che il servizio check-name restituisca 'SUCCESSO'
 # Il nome del sito cambia in maniera tale da arrivare al successo del controllo
-disponibilita = b"USATO"
-while disponibilita == b"USATO":
-    nomeSito += "srs"
+response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
+if(response.content == b"USATO"):
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
     response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
-    disponibilita = response.content
 assert response.status_code == 200
-assert disponibilita == b"DISPONIBILE"
+assert response.content == b"DISPONIBILE"
 
 # Controllo che il servizio create-account-storage restituisca 'SUCCESSO'
 response = requests.post("https://prod-functions-srs.azurewebsites.net/api/create-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
