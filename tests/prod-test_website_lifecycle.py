8f8f63f8da04d390dc0510f3075b051dc3d24278
Francesco Paglia <francesco98paglia@gmail.com>
2023-06-21 20:54:59 +0200

    Aggiunta tentativi automatici nei test

--- tests/prod-test_website_lifecycle.py
+++ tests/prod-test_website_lifecycle.py
@@ -1,48 +1,75 @@
 import requests
-import time
 
 nomeSito = "testwebappsrs"
 gruppoRisorse = "users-env"
+massimoTentativi = 4
 
 # Controllo che il servizio check-name restituisca 'USATO'
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": "teststoragesrs"})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": "prodstoragesrs"})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"USATO"
 print("Controllo disponibilità account di storage già usato eseguito con successo...")
 
 # Controllo che il servizio check-name restituisca 'SUCCESSO'
 # Il nome del sito cambia in maniera tale da arrivare al successo del controllo
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
-if(response.content == b"USATO"):
-    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
     response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
+    if(response.content == b"USATO"):
+        response = requests.post("https://prod-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+        response = requests.post("https://prod-functions-srs.azurewebsites.net/api/check-name", json={"nomeSito": nomeSito})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"DISPONIBILE"
 print("Controllo disponibilità account di storage non ancora usato eseguito con successo...")
 
 # Controllo che il servizio create-account-storage restituisca 'SUCCESSO'
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/create-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/create-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"SUCCESSO"
 print("Creazione dell'account di storage eseguita con successo...")
-time.sleep(60)
 
 # Controllo che il servizio enable-static-website restituisca 'SUCCESSO'
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/enable-static-website", json={"nomeSito": nomeSito})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/enable-static-website", json={"nomeSito": nomeSito})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"SUCCESSO"
 print("Attivazione sito web statico eseguita con successo...")
-time.sleep(60)
 
 # Controllo che il servizio disable-static-website restituisca 'SUCCESSO'
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/disable-static-website", json={"nomeSito": nomeSito})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/disable-static-website", json={"nomeSito": nomeSito})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"SUCCESSO"
 print("Disattivazione sito web statico eseguita con successo...")
-time.sleep(60)
 
 # Controllo che il servizio delete-account-storage restituisca 'SUCCESSO'
-response = requests.post("https://prod-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.post("https://prod-functions-srs.azurewebsites.net/api/delete-account-storage", json={"nomeSito": nomeSito, "gruppoRisorse": gruppoRisorse})
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 200
 assert response.content == b"SUCCESSO"
 print("Eliminazione dell'account di storage eseguita con successo...")
--- tests/test-test_wrong_input_parameters.py
+++ tests/test-test_wrong_input_parameters.py
@@ -1,26 +1,53 @@
 import requests
 
-response = requests.get("https://test-functions-srs.azurewebsites.net/api/check-name")
+massimoTentativi = 4
+
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.get("https://test-functions-srs.azurewebsites.net/api/check-name")
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 400
 assert response.content == b"ERRORE -> Manca il parametro nomeSito"
 print("Funzione check-name testata con successo...")
 
-response = requests.get("https://test-functions-srs.azurewebsites.net/api/create-account-storage")
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.get("https://test-functions-srs.azurewebsites.net/api/create-account-storage")
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 400
 assert response.content == b"ERRORE -> Mancano i parametri nomeSito e gruppoRisorse"
 print("Funzione create-account-storage testata con successo...")
 
-response = requests.get("https://test-functions-srs.azurewebsites.net/api/delete-account-storage")
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.get("https://test-functions-srs.azurewebsites.net/api/delete-account-storage")
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 400
 assert response.content == b"ERRORE -> Mancano i parametri nomeSito e gruppoRisorse"
 print("Funzione delete-account-storage testata con successo...")
 
-response = requests.get("https://test-functions-srs.azurewebsites.net/api/enable-static-website")
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.get("https://test-functions-srs.azurewebsites.net/api/enable-static-website")
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 400
 assert response.content == b"ERRORE -> Manca il parametro nomeSito"
 print("Funzione enable-static-website testata con successo...")
 
-response = requests.get("https://test-functions-srs.azurewebsites.net/api/disable-static-website")
+tentativoAttuale = 1
+risultatoRichiesta = 500
+while tentativoAttuale <= massimoTentativi and risultatoRichiesta != 200:
+    response = requests.get("https://test-functions-srs.azurewebsites.net/api/disable-static-website")
+    risultatoRichiesta = response.status_code
+    tentativoAttuale += 1
 assert response.status_code == 400
 assert response.content == b"ERRORE -> Manca il parametro nomeSito"
 print("Funzione disable-static-website testata con successo...")
